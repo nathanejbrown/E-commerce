6f14b58dec597b447a2ba5943f5d6044f0eb6542
gvickstrom <gvickstrom@gmail.com>
2016-07-28 14:20:59 -0600

    carousel is done, html and js and css up to date as of 220pm

--- src/homepage/main.js
+++ src/homepage/main.js
@@ -1,17 +1,16 @@
 
+// ***click company logo to return to homepage***
 
+$(document).ready(function() {
+  $('img.logo').click(function() {
+    window.location.href = 'index.html';
+  });
+});
 
+//***carousel code***
 
-
-
-
-
-
-
-
-
-//to make the carousel hopefully work someday
 carousel = (function(){
+
   var box = document.querySelector('.carouselbox');
   var next = box.querySelector('.next');
   var prev = box.querySelector('.prev');
@@ -20,7 +19,13 @@ carousel = (function(){
   var amount = items.length;
   var current = items[0];
   box.classList.add('active');
-  function navigate(direction) {
+
+    setInterval(function(){
+      navigate(1)
+    }, 5000)
+
+    function navigate(direction) {
+      console.log(counter)
     current.classList.remove('current');
     counter = counter + direction;
     if (direction === -1 &&
@@ -34,6 +39,7 @@ carousel = (function(){
     current = items[counter];
     current.classList.add('current');
   }
+
   next.addEventListener('click', function(ev) {
     navigate(1);
   });
@@ -42,3 +48,12 @@ carousel = (function(){
   });
   navigate(0);
 })();
+
+// **email validation**
+
+$(document).ready(function() {
+  $(document).on('click', function(e) {
+    event.preventDefault();
+    // if('#email-input' !)
+  })
+})
