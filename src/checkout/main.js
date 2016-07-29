c65f7936c07832666bcfce125d215d35521eda92
Derek Styer <dmstyer@gmail.com>
2016-07-29 11:13:38 -0600

    almost there...

--- src/checkout/main.js
+++ src/checkout/main.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
         $('#billingAddress2').val($('#shippingAddress2').val());
         $('#billingState').val($('#shippingState').val());
         $('#billingZip').val($('#shippingZip').val());
+        $('#billingCity').val($('#shippingCity').val());
     })
 })
 
@@ -59,46 +60,8 @@ $('#shippingState').on('input', function() {
 
 })
 
-
-
-
 //regletter allows only letters and spaces
 var regLetter = /^[A-Za-z\s]+$/;
-
-// $('#shippingName').on('input', function () {
-//   var bool = regLetter.test($('#shippingName').val());
-//     bool === true ? $(this).css("border", "2px solid green") : $(this).css("border", "2px solid red") && $('#purchasebtn').attr('disabled','disabled') && $('#purchasebtn').css("opacity",".25");
-// })
-
-// $('#shippingLastName').on('input', function() {
-//     var bool = regLetter.test($('#shippingLastName').val());
-//     bool === true ? $(this).css("border", "2px solid green") && $('#purchasebtn').removeAttr("disabled") : $(this).css("border", "2px solid red") && $('#purchasebtn').attr('disabled', 'disabled') && $('#purchasebtn').css("opacity", ".25");
-// })
-//
-// $('#billingName').on('input', function() {
-//     var bool = regLetter.test($('#billingName').val());
-//     bool === true ? $(this).css("border", "2px solid green") : $(this).css("border", "2px solid red") && $('#purchasebtn').attr('disabled', 'disabled') && $('#purchasebtn').css("opacity", ".25");
-// })
-//
-// $('#billingLastName').on('input', function() {
-//     var bool = regLetter.test($('#billingLastName').val());
-//     bool === true ? $(this).css("border", "2px solid green") : $(this).css("border", "2px solid red") && $('#purchasebtn').attr('disabled', 'disabled') && $('#purchasebtn').css("opacity", ".25");
-// })
-//
-// $('#shippingAddress1').on('input', function() {
-//     $('#shippingAddress1').val() === '' ? $(this).css("border", "2px solid red") && $('#purchasebtn').attr('disabled', 'disabled') && $('#purchasebtn').css("opacity", ".25") : $(this).css("border", "2px solid green");
-// })
-// $('#billingAddress1').on('input', function() {
-//     $('#billingAddress1').val() === '' ? $(this).css("border", "2px solid red") && $('#purchasebtn').attr('disabled', 'disabled') && $('#purchasebtn').css("opacity", ".25") : $(this).css("border", "2px solid green");
-// })
-//
-// $('#shippingCity').on('input', function() {
-//     $('#shippingCity').val() === "" ? $(this).css("border", "2px solid red") && $('#purchasebtn').attr('disabled', 'disabled') && $('#purchasebtn').css("opacity", ".25") : $(this).css("border", "2px solid green");
-// })
-//
-// $('#billingCity').on('input', function() {
-//     $('#billingCity').val() === "" ? $(this).css("border", "2px solid red") && $('#purchasebtn').attr('disabled', 'disabled') && $('#purchasebtn').css("opacity", ".25") : $(this).css("border", "2px solid green");
-// })
 
 $('#shippingZip').on('input', function() {
     if ($('#shippingZip').val().length < 5 || $('#shippingZip').val().length > 10) {
@@ -115,16 +78,6 @@ $('#billingZip').on('input', function() {
         $(this).css("border", "2px solid green");
     }
 })
-
-// $('#shippingState').on('input', function() {
-//     console.log($('#shippingState').val());
-//     $('#shippingState').val() === "" ? $(this).css("border", "2px solid red") && $('#purchasebtn').attr('disabled', 'disabled') && $('#purchasebtn').css("opacity", ".25") : $(this).css("border", "2px solid green") && $(this).removeAttr('disabled');
-//
-// })
-
-// $('#billingState').on('input', function() {
-//     $('#billingState').val() === "" ? $(this).css("border", "2px solid red") && $('#purchasebtn').attr('disabled', 'disabled') && $('#purchasebtn').css("opacity", ".25") : $(this).css("border", "2px solid green");
-// })
 
 
 //credit card validation
@@ -146,9 +99,29 @@ $('#cvc').on('input', function() {
 
     $('#cvc').length === 0 ? $(this).css("border", "2px solid red") && $('#purchasebtn').attr('disabled', 'disabled') && $('#purchasebtn').css("opacity", ".25") : $(this).css("border", "2px solid green")
     $('#purchasebtn').removeAttr("disabled");;
+  });
 
+  function getRandomNumber(ceilingNumber) {
+    return Math.ceil(Math.random() * ceilingNumber);
+  }
 
+$(document).ready(function (){
+    var productPictures = ['http://cdn.thisiswhyimbroke.com/images/hamburger-pet-bed1-640x533.jpg',
+         'http://www.ggloryind.com/image/PetBed.jpg"class="0',
+         'http://g01.a.alicdn.com/kf/HTB1zzVNKVXXXXbSXXXXq6xXFXXXG/1PCS-Hot-font-b-Dog-b-font-font-b-Bed-b-font-Pet-Products-Warm-Soft.jpg',
+         'http://odditymall.com/includes/content/shoe-shaped-dog-bed-thumb.jpg',
+         'http://www.bingpet.com/upload/product/goods_main__1378738148.jpg',
+         'http://ledies-first.ru/wp-content/uploads/2012/03/fileQKv1N.jpg.zoom_.jpg',
+         'https://cmgpbpmalled.files.wordpress.com/2015/09/star-wars-captain-phasmae284a2-rope-wrap-dog-toy-12-99-image-23.jpg?w=300&h=300',
+         'http://www.insidethemagic.net/wp-content/uploads/2015/08/800443980743C.jpg',
+         'http://www.insidethemagic.net/wp-content/uploads/2015/08/800443980835C.jpg',
+         'http://www.livbit.com/article/wp-content/uploads/2014/10/humungadogtoy_1.jpg',
+         'https://www.rover.com/blog/wp-content/uploads/2015/04/r2dog2-boxer-star-wars-day.jpg',
+         'http://media.gadgetsin.com/2016/03/handmade_star_wars_dog_clothes_disguise_your_pet_as_bb8_r2d2_or_c3po_1.jpg',
+         'http://a.dilcdn.com/bl/wp-content/uploads/sites/6/2015/10/Bantha-Dog.jpg',
+         'http://www.smartshopperteam.com/wp-content/uploads/2016-Hot-Sale-modelling-of-Minions-micky-Dog-Costume-Novelty-Funny-Halloween-Party-Pet-Dog-Clothes-2.jpg'];
 
+var everyProduct = []
 
     Promise.resolve($.ajax({
         url: 'http://galvanize-student-apis.herokuapp.com/gcommerce/products'
@@ -158,12 +131,8 @@ $('#cvc').on('input', function() {
           product.rating = getRandomNumber(5);
           everyProduct.push(product);
         });
-        price(everyProduct);
-        size(everyProduct);
       }).then(function() {
         var sales = getAnyNumberOfProducts(3);
-        var items = getAnyNumberOfProducts(10);
-        addProductInfoToPage(items);
         addSaleItems(sales);
       }).catch(function(error) {
         console.log(error);
@@ -176,6 +145,7 @@ function addSaleItems(array) {
   }
 }
 function initialize() {
+  console.log('hi');
     var mapProp = {
       center: new google.maps.LatLng(39.733513, -104.992588),
       zoom:14,
@@ -189,6 +159,17 @@ function initialize() {
   }
   google.maps.event.addDomListener(window, 'load', initialize);
 
+  function getAnyNumberOfProducts(num) {
+    var randomProducts = [];
+    var productsClone = everyProduct.slice(0);
+    for (i = 0; i < num; i++) {
+      var random = getRandomNumber(productsClone.length - 1);
+      randomProducts.push(productsClone[random]);
+      productsClone.splice(random, 1);
+    }
+    return randomProducts;
+  }
+
   function makeSomeStars(num) {
     var stars = [];
     for (i = 0; i < num; i++) {
@@ -199,9 +180,3 @@ function initialize() {
   }
 
 });
-// function validate(){
-//   if($(this).css("border") === '2px solid rgb(255, 0, 0)') {
-//     $('#purchasebtn').removeAttr('disabled')
-//     $('#purchasebtn').css("opacity","1")
-//       }
-// }
