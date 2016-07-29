3f4f1c3413afb5057161987acf7ebc885bf85929
Heathcliff <dennis.nathan@gmail.com>
2016-07-29 11:51:29 -0600

    removed conflicts

--- src/products/main.js
+++ src/products/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $('document').ready(function() {
   var everyProduct = [];
   var productPictures = ['http://cdn.thisiswhyimbroke.com/images/hamburger-pet-bed1-640x533.jpg',
@@ -79,29 +78,7 @@ $('document').ready(function() {
       var num = parseInt(this.value);
       var product = products.filter(function (productPrice) {
         return productPrice.price.replace('$', '') < num;
-=======
-$(document).ready(function () {
 
-// data from ajax
-  function sortBy(){
-    var promise = Promise.resolve($.ajax({
-      url:'http://galvanize-student-apis.herokuapp.com/gcommerce/products',
-      method: 'get'
-    }))
-    .then(function(data){
-      price(data);
-      size(data);
-    })
-  }
-  sortBy();
-
-// filter price
-  function price(data) {
-    $('.priceB button').click(function () {
-      var num = parseInt(this.value);
-      var product = data.filter(function (productPrice) {
-        return productPrice.price.replace('$', '') < num
->>>>>>> master
       });
       if (product.length > 10) {
         product = product.slice(0, 10);
@@ -110,20 +87,11 @@ $(document).ready(function () {
     });
   }
 
-<<<<<<< HEAD
   function size(products) {
     $('.sizeB button').click(function() {
       var num = parseInt(this.value);
       var product = products.filter(function (productSize) {
         return productSize.size === num;
-=======
-// filter size
-  function size(data) {
-    $('.sizeB button').click(function(){
-      var num = parseInt(this.value);
-      var product = data.filter(function (productSize) {
-        return  productSize.size === num;
->>>>>>> master
       });
       if (product.length > 10) {
         product = product.slice(0, 10);
@@ -132,7 +100,6 @@ $(document).ready(function () {
     });
   }
 
-<<<<<<< HEAD
   function initialize() {
     var mapProp = {
       center: new google.maps.LatLng(39.733513, -104.992588),
@@ -157,18 +124,3 @@ $(document).ready(function () {
   }
 
 });
-=======
-// add pic, price, description to products div
-  function addProductInfoToPage(array) {
-    $('#productInfo').empty();
-    for (item in array) {
-      $('#productInfo').append('<img>');
-      $('#productInfo img').attr('src', 'http://placehold.it/250x250');
-      $('#productInfo').append('<h3> ' + [item] + ' </h3>');
-      $('#productInfo').append('<p class="price"> ' + array[item].price + ' </p>');
-      $('#productInfo').append('<p class="description"> ' + array[item].description + ' </p>');
-    }
-  }
-
-}); // end $(document).ready
->>>>>>> master
